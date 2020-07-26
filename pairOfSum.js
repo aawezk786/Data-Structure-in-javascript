4f6425e5814c17e75b82bfabbc1b1d7dfdfaa987
aawezk456 <aawezk456@gmail.com>
2020-07-26 17:38:36 +0530

    pair of sum in array updated

---
+++ pairOfSum.js
@@ -0,0 +1,20 @@
+function twosums(nums, target) {
+    numsIndexes = {};
+  
+    for (let i = 0; i < nums.length; i += 1) {
+      let currentDifference = target - nums[i];
+  
+      if (numsIndexes[currentDifference] !== undefined && numsIndexes[currentDifference] !== i) {
+        return [i, numsIndexes[currentDifference]];
+      } else {
+        numsIndexes[nums[i]] = i;
+      }
+
+      if (numsIndexes[currentDifference] == undefined) {
+        return false
+      }
+    }
+  };
+
+ var result =  twosums([50,20,40,50,60,70],50);
+  console.log(result);
